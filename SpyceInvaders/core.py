91eadd15949ed700e251a8b011cb0b4c927d375a
Koci0 <tomasz.kot13@gmail.com>
2020-02-27 20:29:05 +0100

    Add View class with background and rectangle drawing

--- SpyceInvaders/core.py
+++ SpyceInvaders/core.py
@@ -1,20 +1,53 @@
-import pygame
 import sys
+
+import pygame
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+
+class View(object):
+
+    def __init__(self, width=800, height=400, fps=60):
+        pygame.init()
+        pygame.display.set_caption("Press ESC to quit")
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.background = pygame.Surface(self.screen.get_size()).convert()
+        self.background.fill(black)
+        self.clock = pygame.time.Clock()
+        self.fps = fps
+        self.font = pygame.font.SysFont("mono", 24)
+
+    def paint(self):
+        pygame.draw.rect(self.background, white, (200, 150, 50, 50))
+
+    def run(self):
+        self.paint()
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+
+            self.clock.tick(self.fps)
+            self.draw_text("FPS: {:6.3}".format(self.clock.get_fps()))
+
+            pygame.display.flip()
+            self.screen.blit(self.background, (0, 0))
+
+        pygame.quit()
+
+    def draw_text(self, text):
+        fw, fh = self.font.size(text)
+        surface = self.font.render(text, True, white)
+        self.screen.blit(surface, (0, 0))
 
 
 def run():
-    pygame.init()
-
-    size = (width, height) = 800, 600
-    black = (0, 0, 0)
-
-    screen = pygame.display.set_mode(size)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        screen.fill(black)
-        pygame.display.flip()
+    View().run()
+    sys.exit()
