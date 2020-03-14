e5fb058408efaabeb7bb84a766804099914e2344
Koci0 <tomasz.kot13@gmail.com>
2020-03-14 12:25:01 +0100

    Update Actor and Entity parameters

--- SpyceInvaders/actor.py
+++ SpyceInvaders/actor.py
@@ -3,8 +3,19 @@ import pygame
 
 class Actor(object):
 
-    def __init__(self, name, x, y):
+    def __init__(self, name, x, y, destructible=False, hp=None):
         self.image = pygame.image.load("data/" + str(name) + ".png").convert_alpha()
         self.rectangle = self.image.get_rect()
         self.x = x
         self.y = y
+        self.destructible = destructible
+        self.hp = hp
+
+    def receive_damage(self, damage):
+        if self.destructible and self.hp is not None:
+            self.hp = self.hp - damage
+
+    def is_alive(self):
+        if self.hp is not None and self.hp <= 0:
+            return False
+        return True
--- SpyceInvaders/entity.py
+++ SpyceInvaders/entity.py
@@ -4,12 +4,17 @@ from SpyceInvaders.actor import Actor
 
 class Entity(Actor, object):
 
-    def __init__(self, name, x, y, speed=0):
+    def __init__(self, name, x, y, speed=0, cooldown=0):
         super().__init__(name, x, y)
         self.speed = speed
+        self.cooldown = cooldown
 
     def move(self, direction):
         if direction == "left" and self.x > 0:
             self.x = self.x - self.speed
-        if direction == "right" and self.x <= settings.screen_width - self.rectangle.width:
+        elif direction == "right" and self.x < settings.screen_width - self.rectangle.width:
             self.x = self.x + self.speed
+        elif direction == "down" and self.y < settings.screen_height - self.rectangle.height:
+            self.y = self.y + self.speed
+        elif direction == "up" and self.y > 0:
+            self.y = self.y - self.speed
