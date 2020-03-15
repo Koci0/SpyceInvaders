7e145d1fcb7f001b8ff9931c3093ac4e3d788872
Koci0 <tomasz.kot13@gmail.com>
2020-03-15 11:44:29 +0100

    Add Bullet class. Add Player ability to shoot

--- SpyceInvaders/player.py
+++ SpyceInvaders/player.py
@@ -1,8 +1,22 @@
+from pygame.time import get_ticks
+
+from SpyceInvaders.bullet import Bullet
 from SpyceInvaders.entity import Entity
 
 
 class Player(Entity, object):
 
     def __init__(self, x, y, name="player"):
-        super().__init__(name, x, y)
-        self.speed = 5
+        super().__init__(name, x, y, speed=5, cooldown=100)
+        self.last_shot = get_ticks()
+
+    def shoot(self, direction="up"):
+        now = get_ticks()
+        if now - self.last_shot >= self.cooldown:
+            self.last_shot = now
+            return self.spawn_bullet(direction)
+        return None
+
+    def spawn_bullet(self, direction):
+        bullet = Bullet(self.x + 0.5 * self.rectangle.width, self.y, direction)
+        return bullet
