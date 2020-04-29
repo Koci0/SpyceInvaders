2f7ef9798b16feb34db1cced5a26ef776ca2b7f2
Koci0 <tomasz.kot13@gmail.com>
2020-04-29 17:36:12 +0200

    Resolve issues #1

--- SpyceInvaders/alien.py
+++ SpyceInvaders/alien.py
@@ -1,14 +1,14 @@
-import SpyceInvaders.settings as settings
+from SpyceInvaders import settings
 from SpyceInvaders.bullet import Bullet
 from SpyceInvaders.entity import Entity
 
 
-class Alien(Entity, object):
+class Alien(Entity):
 
-    def __init__(self, x, y, direction="left", name="alien"):
-        super().__init__(name, x, y)
+    def __init__(self, x, y, direction="left", filename="alien.png"):
+        super().__init__(filename, x, y)
         self.destructible = True
-        self.hp = settings.alien_hp
+        self.hp = settings.ALIEN_HP
         self.speed = 1.0
         self.direction = direction
 
@@ -18,10 +18,10 @@ class Alien(Entity, object):
         return False
 
     def swap_direction(self):
-        if self.direction == "left":
-            self.direction = "right"
+        if self.direction == settings.LEFT:
+            self.direction = settings.RIGHT
         else:
-            self.direction = "left"
+            self.direction = settings.LEFT
 
     def spawn_bullet(self, direction, bullet_type):
         bullet = Bullet(self.rectangle.x + 0.5 * self.rectangle.width, self.rectangle.y, direction, bullet_type)
--- SpyceInvaders/entity.py
+++ SpyceInvaders/entity.py
@@ -1,9 +1,8 @@
-import SpyceInvaders.settings as settings
-
+from SpyceInvaders import settings
 from SpyceInvaders.actor import Actor
 
 
-class Entity(Actor, object):
+class Entity(Actor):
 
     def __init__(self, name, x, y, speed=0, cooldown=0):
         super().__init__(name, x, y)
@@ -11,18 +10,17 @@ class Entity(Actor, object):
         self.cooldown = cooldown
 
     def move(self, direction, steps=1):
-        if direction == "left" and self.rectangle.x > 0:
+        if direction == settings.LEFT and self.rectangle.x > 0:
             self.rectangle.x = self.rectangle.x - self.speed * steps
-        elif direction == "right" and self.rectangle.x < settings.screen_width - self.rectangle.width:
+        elif direction == settings.RIGHT and self.rectangle.x < settings.SCREEN_WIDTH - self.rectangle.width:
             self.rectangle.x = self.rectangle.x + self.speed * steps
-        elif direction == "down" and self.rectangle.y < settings.screen_height - self.rectangle.height:
+        elif direction == settings.DOWN and self.rectangle.y < settings.SCREEN_HEIGHT - self.rectangle.height:
             self.rectangle.y = self.rectangle.y + self.speed * steps
-        elif direction == "up" and self.rectangle.y > 0:
+        elif direction == settings.UP and self.rectangle.y > 0:
             self.rectangle.y = self.rectangle.y - self.speed * steps
         else:
             return True
-        return False
 
     def receive_damage(self, _):
         if self.destructible and self.hp is not None:
-            self.hp = self.hp - settings.bullet_damage
+            self.hp = self.hp - settings.BULLET_DAMAGE
